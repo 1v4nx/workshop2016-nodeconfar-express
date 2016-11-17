3f2379cc2bb1e8b31bc6ce76634363f0c4af2ab8
Diego Villagran <diegovillagran@ARBUEOSXR1021.local>
2016-11-17 10:57:04 -0300

    artist model

--- api/models/artist-model.js
+++ api/models/artist-model.js
@@ -1,3 +1,14 @@
-/**
- * Created by Josue on 17/11/16.
- */
+import {database, docTypes} from '../../database';
+
+export class ArtistModelClass {
+    getList() {
+        return database.find({docType: docTypes.ARTIST})
+    }
+
+    getById(id) {
+        return database.findOne({docType: docTypes.ARTIST, '_id': id})
+    }
+
+}
+
+export const artistModel = new ArtistModelClass();
