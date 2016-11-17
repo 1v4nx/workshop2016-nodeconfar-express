75ad65bc170a80a9bc70ba90471694cec5f6f265
Braian Iván Monnier <bmonnier@medallia.com>
2016-11-17 11:53:29 -0300

    Add missing models

--- api/models/index.js
+++ api/models/index.js
@@ -1,3 +1,5 @@
 export * from './track-model';
 export * from './band-model';
-export * from './album-model'
+export * from './album-model';
+export * from './artist-model';
+export * from './comment-model';
